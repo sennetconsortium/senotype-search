a18d0f69dbd301a6e4ff3f77990764f358f13edd
maxsibilla <maxsibilla@icloud.com>
2026-04-10 11:12:55 -0400

    Correcting imports

--- src/components/Senotype/ViewSenotype.jsx
+++ src/components/Senotype/ViewSenotype.jsx
@@ -1,4 +1,4 @@
-import AppAccordion from '@/components/layout/AppAccordion';
+import AppAccordion from '@/components/AppAccordion';
 import React, { useRef, useState } from 'react';
 import { LinkOutlined, SearchOutlined } from '@ant-design/icons';
 import { Button, Col, Descriptions, Input, Row, Space, Table } from 'antd';
@@ -8,7 +8,7 @@ import {
   getSciCrunchUrl,
 } from '@/lib/senotype';
 import ClipboardCopy from '@/components/ClipboardCopy';
-import AppAnchor from '@/components/layout/AppAnchor';
+import AppAnchor from '@/components/AppAnchor';
 
 const buildAssertionChildren = (assertions, term) => {
   return assertions
