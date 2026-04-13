97256b7ab7277fb5b7d14ad2551184221bc101e6
Lisa-Ann B <lisabruney@pitt.edu>
2026-04-13 14:07:15 -0400

    Add function docstring

--- src/components/AppAccordion.jsx
+++ src/components/AppAccordion.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 
+
+/**
+ * Description placeholder
+ *
+ * @param {{ title: any; id: any; children: any; className?: string; }} props 
+ * @param {string} props.title The title of the accordion
+ * @param {string} props.id The css id of the accordion
+ * @param {node} props.children 
+ * @param {string} [props.className='mt-4'] 
+ * @returns {*} 
+ */
 function AppAccordion({ title, id, children, className = 'mt-4' }) {
   return (
     <div className={`c-accordion ${className}`}>
--- src/components/AppFloatingButton.jsx
+++ src/components/AppFloatingButton.jsx
@@ -2,6 +2,18 @@ import { FloatButton } from 'antd';
 import { LeftCircleOutlined, RightCircleOutlined } from '@ant-design/icons';
 import THEME from '@/lib/theme';
 
+
+/**
+ * Displays a floating button
+ *
+ * @param {{ show: any; setShow: any; onClick: any; text: any; buttonStyle?: { top: number; left: number; right: string; bottom: string; }; }} props 
+ * @param {useState.boolean} props.show useState boolean variable
+ * @param {useState.function} props.setShow useState method used to toggle the value of show
+ * @param {function(event)} props.onClick A fuction to call on click of button
+ * @param {string} props.text Text describing action
+ * @param {{ top: number; left: number; right: string; bottom: string; }} [props.buttonStyle={ top: 350, left: 10, right: 'auto', bottom: 'auto' }] 
+ * @returns {*} 
+ */
 function AppFloatingButton({
   show,
   setShow,
