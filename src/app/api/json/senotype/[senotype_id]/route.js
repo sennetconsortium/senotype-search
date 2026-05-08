aaa520616d5e27e87058c54cc62abc4b720fa9cf
Lisa-Ann B <lisabruney@pitt.edu>
2026-05-08 14:59:29 -0400

    DRY code

--- src/app/api/json/senotype/[senotype_id]/route.js
+++ src/app/api/json/senotype/[senotype_id]/route.js
@@ -1,26 +1,17 @@
 import { NextResponse } from 'next/server';
 import API from '@/lib/api';
 import { cookies, headers } from 'next/headers';
-import { getCookie } from 'cookies-next';
+import AUTH from '@/lib/auth';
 
 export async function GET(request, { params }) {
-  let groupsToken;
-
-  // First check if Bearer token is passed in the header
   const { senotype_id } = await params;
-  const authHeader = (await headers()).get('authorization');
-  if (authHeader && authHeader.startsWith('Bearer ')) {
-    groupsToken = authHeader.split(' ')[1];
+  const _cookies = await cookies();
+  let token = await AUTH.tokenFromHeader(headers)
+  if (!token) {
+    token = AUTH.token(_cookies)
   }
-  // Try to grab the token from cookies
-  if (!groupsToken) {
-    const info = await getCookie('info', { cookies });
-    if (info) {
-      const auth = JSON.parse(atob(info));
-      groupsToken = auth?.groups_token;
-    }
-  }
-  const data = await API.fetchSenotype(senotype_id, groupsToken);
+
+  const data = await API.fetchSenotype(senotype_id, token);
   if (data && data.hasOwnProperty('error')) {
     return NextResponse.json(
       { error: data['error'] },
