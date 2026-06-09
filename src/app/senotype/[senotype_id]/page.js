e944c8ba5fc8ca5cd1f5f6a7d322834df0cf13be
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-06-09 13:21:03 -0400

    Replenish component

--- src/app/senotype/[senotype_id]/page.js
+++ src/app/senotype/[senotype_id]/page.js
@@ -1,15 +1,51 @@
-import SenotypeClientComponent from './SenotypeClientComponent';
+'use client';
 
-export async function generateMetadata({ params }) {
-  const { senotype_id } = await params;
+import { useParams } from 'next/navigation';
+import BasicLayout from '@/components/layout/BasicLayout';
+import { useSenotype } from '@/hooks/useFetchSenotype';
+import ViewSenotype from '@/components/senotype/ViewSenotype';
+import AppSpinner from '@/components/AppSpinner';
+import { useContext } from 'react';
+import AppContext from '@/context/AppContext';
+import NotFound from '@/components/errors/NotFound';
+import Unauthorized from '@/components/errors/Unauthorized';
 
-  return {
-    title: `Senotype`,
-  };
-}
+export default function Page() {
+  const { auth } = useContext(AppContext);
 
-export default async function Page({ params }) {
-  const { senotype_id } = await params;
+  const params = useParams();
+  const senotype_id = params.senotype_id;
 
-  return <SenotypeClientComponent senotype_id={senotype_id} />;
+  const { data, loading, error } = useSenotype(senotype_id);
+
+  if (!loading && error) {
+    throw error;
+  }
+
+  return (
+    <BasicLayout>
+      <>
+        {loading && (
+          <>
+            <AppSpinner />
+          </>
+        )}
+        {/*If not data is returned from Search API and user is not logged in assume 401 otherwise 404*/}
+        {!loading && !data && (
+          <>
+            {auth.isAuthenticated && auth.hasSenotypeEdit ? (
+              <NotFound />
+            ) : (
+              <Unauthorized />
+            )}
+          </>
+        )}
+        {data && (
+          <>
+            <ViewSenotype senotype={data} />
+          </>
+        )}
+      </>
+    </BasicLayout>
+  );
 }
