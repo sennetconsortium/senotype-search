464dd0cb1d4b37e9dae4b71b937f4153a3c060fb
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-06-09 16:18:20 -0400

    Update organ facet

--- src/lib/search.js
+++ src/lib/search.js
@@ -1,33 +1,7 @@
 import AUTH from './auth';
 import log from 'xac-loglevel';
-import { ontology } from '@/cache/ontology';
 
 const SEARCH = {
-  organBucketsTransform: (ops) => {
-    const { aggregations, field, component } = ops;
-    const buckets = SEARCH.bucketsTransform(ops);
-    log.debug(
-      'SEARCH.organBucketsTransform',
-      component,
-      field,
-      aggregations[field],
-    );
-
-    let organs = {};
-    let h;
-    for (const b of buckets) {
-      h = ontology.organ_types.hierarchy[b.key] || 'Unknown';
-      organs[h] = organs[h] || {
-        doc_count: 0,
-        key: h,
-        subagg: { buckets: [] },
-      };
-      organs[h].doc_count += b.doc_count;
-      organs[h].subagg.buckets.push(b);
-    }
-
-    return Object.values(organs);
-  },
   bucketsTransform: (ops) => {
     const { aggregations, field } = ops;
     log.debug('SEARCH.bucketsTransform', field, aggregations[field]);
